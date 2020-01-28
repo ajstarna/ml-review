4eeac95afe894c39dbe445a244b680fdb06de1ca
Adam St Arnaud <ajstarna@ualberta.ca>
2020-01-27 23:00:30 -0700

    added a file with functions for evaluating a model on a data set. cross validation and p,r,f so far

---
+++ mlreview/utils/evaluation.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def p_r_f(predictions, Y):
+    '''
+    Returns precision, recall, and F1 score
+    '''
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for pred, y in zip(predictions, Y):
+        if pred == y:
+            if pred == 1:
+                tp += 1
+            else:
+                tn += 1
+        else:
+            if pred == 1:
+                fp += 1
+            else:
+                fn += 1
+    p = tp / (tp + fp)
+    r = tp / (tp + fn)
+    f = 2*p*r / (p + r)
+
+    return p,r,f
+
+def KFolds(X, Y, num_folds=5):
+    # get a list representing indices into X/Y, then shuffle the indices
+    indices = np.arange(len(Y))
+    np.random.shuffle(indices)
+
+    for i in range(num_folds):
+        # For each fold of cross val, grab the next subset of data
+        # to be the hold-out portion. Since the indices were pre-shuffled,
+        # we can just go in order
+        start_index = int(i*len(Y)/num_folds)
+        #print(f'start_index = {start_index}')
+        end_index = int(start_index + len(Y)/num_folds)
+        #print(f'end_index = {end_index}')
+        indices_for_holdout = indices[start_index: end_index]
+        #print(f'indices_for_holdout = {indices_for_holdout}')
+        first_chunk = indices[0:start_index]
+        second_chunk = indices[end_index:]
+        indices_for_training = np.concatenate((first_chunk, second_chunk))
+
+        training_X, training_Y = X[indices_for_training],  Y[indices_for_training]
+        holdout_X, holdout_Y = X[indices_for_holdout],  Y[indices_for_holdout]
+
+        yield training_X, training_Y, holdout_X, holdout_Y
+
+def cross_validation(model, X, Y, num_folds=5):
+    # given a model that can completely transform and predict the given data, X
+    # and the expected output, Y, returns a dictionary of evaluation metrics
+    # for evaluating num_folds of cross validation
+
+    predictions = np.array([])
+    truth = np.array([])
+    for training_X, training_Y, holdout_X, holdout_Y in KFolds(X, Y, num_folds=num_folds):
+        model.fit(training_X, training_Y)
+        current_predictions = model.predict(holdout_X)
+        predictions = np.concatenate((predictions, current_predictions))
+        truth = np.concatenate((truth, holdout_Y))
+
+    p,r,f = p_r_f(predictions, truth)
+    print(f'predictions = {predictions}')
+    print(f'truth = {truth}')
+    print(f'p={p}, r={r}, f={f}')
+
